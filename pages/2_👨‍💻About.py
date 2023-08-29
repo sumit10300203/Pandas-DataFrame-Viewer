87525a7e0ecacb97ba59a74aa8e789d915f450dc
Sumit Dhar <66067910+sumit10300203@users.noreply.github.com>
2023-08-29 13:32:32 +0530

    Update 2_👨‍💻About.py

--- pages/2_👨‍💻About.py
+++ pages/2_👨‍💻About.py
@@ -17,10 +17,6 @@ with col[0].container():
     st.markdown('''##### Hi, I am Sumit 👋
 ### A Data Analyst Aspirant From India
 **I am passionate about Data Analysis, Data Visualization, Machine Learning, Frontend and Backend Developing, Data Structures and Algorithms.**
-
-* **Connect with me on [LinkedIn >](https://bit.ly/3DyD6cP)**            
-* **Mail me on sumit10300203@gmail.com**
-* **Please leave us your Feedback on [Feedback G-Form>](https://forms.gle/vzVN6h7FtwCn45hw6)**
 ''')
 
 with col[1].container():
@@ -34,9 +30,10 @@ with col[0].container():
 
 **v1.0 Beta**
 
+* **This website can be used for Data Analysis, Data Filtering, Data Modifying and Data Visualization purposes.**
 * **This Project is solely inspired by my experience with the [PandasGUI]((https://github.com/adamerose/PandasGUI)) Library.**
 * **Re-created most of the functions of PandasGUI library.**
-* **Libraries Used: [Streamlit](https://streamlit.io/), [Streamlit_extras](https://extras.streamlit.app/), [Pandas](https://pandas.pydata.org/), [Numpy](https://numpy.org/), [Plotly-chart](https://plotly.com/), [Wordcloud](https://amueller.github.io/word_cloud/).**
+* **Libraries Used: [Streamlit](https://streamlit.io/), [Streamlit_extras](https://extras.streamlit.app/), [Pandas](https://pandas.pydata.org/), [Numpy](https://numpy.org/), [Plotly](https://plotly.com/), [Wordcloud](https://amueller.github.io/word_cloud/).**
 * **Stores data in browser's cache.**
 * **Open Source.**
 * **As this project is in beta stage, if you find any :red[errors] please send me a screenshot in the feedback form.**
@@ -52,3 +49,10 @@ with col[1].container():
     st.image("images/4002785.png", width = 450, clamp = True)
 
 st.divider()
+
+with col[0].container():
+    st.markdown('''
+* **Connect with me on [LinkedIn >](https://bit.ly/3DyD6cP)**            
+* **Mail me on sumit10300203@gmail.com**
+* **Please leave us your Feedback on [Feedback G-Form>](https://forms.gle/vzVN6h7FtwCn45hw6)**
+''')
